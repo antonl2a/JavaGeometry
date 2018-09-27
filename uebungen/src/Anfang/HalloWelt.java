620422d7ca2fc258cc4b2852ee6be36f227eff92
201218026 <201218026@STUDWS208-08.FDIBA.local>
2018-09-27 08:26:57 +0300

    123

---
+++ uebungen/src/Anfang/HalloWelt.java
@@ -0,0 +1,7 @@
+package Anfang;
+
+public class HalloWelt {
+	public static void main(String[] args) {
+		System.out.println("Hallo Welt");
+	}
+}
---
+++ uebungen/src/Anfang/Weihnachtsbaum.java
@@ -0,0 +1,12 @@
+package Anfang;
+
+public class Weihnachtsbaum {
+	public static void main(String[] args) {
+		System.out.println("    *");
+		System.out.println("   ***");
+		System.out.println("  *****");
+		System.out.println(" *******");
+		System.out.println("*********");
+		System.out.println("   ***");
+	}
+}
