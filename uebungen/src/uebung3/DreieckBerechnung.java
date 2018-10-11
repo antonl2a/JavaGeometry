c85214e32080e3f856bdaad340741d54edff3710
201218026 <201218026@STUDWS208-07.FDIBA.local>
2018-10-11 09:04:40 +0300

    213

--- uebungen/src/uebung3/DreieckBerechnung.java
+++ uebungen/src/uebung3/DreieckBerechnung.java
@@ -7,12 +7,26 @@ public class DreieckBerechnung {
 		double b=input.nextDouble();
 		double c=input.nextDouble();
 		
-		while((a>0 && b>0 && c>0)&&((a+b>c)||(b+c>a)||(a+c>b)))
+		do {
+		if((a>0 && b>0 && c>0)&&((a+b>c)||(b+c>a)||(a+c>b)))
 		{
 			double p= (a+b+c)/2;
 			double area = Math.sqrt(p*(p-a)*(p-b)*(p-c));
 			System.out.println(area);
+			if(a==b&&a==c&&b==c){
+				System.out.println("gleichseitig");
+				
+			}else if(a==b||a==c||b==c) {
+				System.out.println("gleichschenklig");
+				
+			}else {
+				System.out.println("ungleichseitig");
+			}
+		}else {
+			System.out.println("Inkorrekte Werte");
 		}
+		}while(a>0 && b>0 && c>0);
+		
 		
 		
 				
